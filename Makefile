ba8d8a68dffcf125d7ef00ec1d77c278f80a77eb
Philippe Proost Personal <proostphilippe@gmail.com>
2017-06-23 00:24:21 +0200

    hello world with user input

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: hello_world.c
+	gcc -Wall -Iinclude -o hello_world hello_world.c
---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define BUFFERSIZE 10
+
+int main(void)
+{
+    int number;
+    puts("Please give me some text");
+    char buffer[BUFFERSIZE];
+    fgets(buffer, BUFFERSIZE, stdin);
+    printf("The number is %s \n", buffer);
+   return 0;
+}
+
