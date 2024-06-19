006620150294c9ddcc17949c54a4782a0b3dd2ea
philippe <proostphilippe@gmail.com>
2024-06-19 02:33:15 +0200

    added timing

--- interactive-cat/src/icat.c
+++ interactive-cat/src/icat.c
@@ -2,20 +2,28 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/time.h>
 #define BUFFERSIZE 50
 
 
-int cat(FILE* fd)
+int cat(FILE* fd, size_t buffsize)
 {
-	char buffer[BUFFERSIZE];
-	size_t bytes = fread(buffer, sizeof(char), BUFFERSIZE, fd);
-	
+	struct timeval tval_before, tval_after, tval_result;
+	gettimeofday(&tval_before, NULL);
+
+	char buffer[buffsize];
+	size_t bytes = fread(buffer, sizeof(char), buffsize, fd);
+
 	while(bytes > 0){
 		fwrite(buffer, sizeof(char), bytes, stdout);
-		bytes = fread(buffer, sizeof(char), BUFFERSIZE, fd);
+		bytes = fread(buffer, sizeof(char), buffsize, fd);
 	}
 
-	fputs("\nEOF\n", stdout);
+	printf("\nEOF\n");
+
+	gettimeofday(&tval_after, NULL);
+	timersub(&tval_after, &tval_before, &tval_result);
+	printf("Time elapsed: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
 	return 0;
 }
 
@@ -24,6 +32,7 @@ int main(void)
 {
 	char buffer[BUFFERSIZE]="";
 	FILE* fd;
+	FILE* fd2;
 
 	while(true)
 	{
@@ -45,13 +54,15 @@ int main(void)
 		}
 
 		fd = fopen(buffer, "r");
+		fd2 = fopen(buffer, "r");
 		if(fd == NULL)
 		{
 			printf("The file %s couldn't be opened.\n", buffer);
 			continue;
 		}
 		printf("Will print contents of %s\n", buffer);
-		cat(fd);
+		cat(fd, 100);
+		cat(fd2, 1);
 	}
 	return 0;
 }
