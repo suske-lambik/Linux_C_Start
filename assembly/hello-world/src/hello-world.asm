869396d89d26bdd23502c6a6bc133d4af8a582a1
Philippe Proost <philippe.proost@police.belgium.eu>
2024-12-02 10:02:14 +0100

    extra comments

--- assembly/hello-world/src/hello-world.asm
+++ assembly/hello-world/src/hello-world.asm
@@ -1,16 +1,16 @@
 .global _start
 
 .section .data
-message: .ascii "Hello world\n"
+message: .ascii "Hello world using AT&T syntax.\n"
 
 .section .text
 _start:
-    mov $1, %rax
-    mov $1, %rdi
-    mov $message, %rsi
-    mov $15, %rdx
-    syscall
+    mov $1, %rax        # syscall 1 is sys_write in x86_64
+    mov $1, %rdi        # file descriptor 1 is stdout
+    mov $message, %rsi  # 
+    mov $32, %rdx       # length of message 
+    syscall             # execute syscall with arguments in the required registers
 
-    mov $60, %rax
-    mov $0, %rdi
+    mov $60, %rax       # syscall 60 is sys_exit in x86_64
+    mov $0, %rdi        # exit status is 0
     syscall
